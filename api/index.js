b27d7561e989c2ae77730daf1e782482ec9e4414
EnuguAbhishek1306 <abhishekreddy1306@gmail.com>
2024-06-11 18:46:51 +0530

    create a middle war and a fucton to handle possoble errors

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';// Explicit extension for ES modules
 import authRouter from './routes/auth.route.js'; // Explicit extension for ES modules
 
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -14,5 +15,13 @@ const app = express();
 app.use(express.json())
 app.use("/api/user", userRouter); 
 app.use("/api/auth",authRouter)
-
+app.use((err,req,res,next)=>{
+    const statuscode=err.statuscode||500;
+    const message=err.message||'internal server error'
+    return res.status(statuscode).json({
+        success:false,
+        statuscode,
+        message
+    })
+})
 app.listen(8000, () => console.log('Server is running on port 8000'));
